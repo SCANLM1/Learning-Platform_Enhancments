39dbb118e37890f6ea70e186d9a2ad1a0d2111a9
Joël Hillmann <joelraine8@gmail.com>
2025-04-03 12:46:50 +1300

    Imported merge changes from previous repository
    Drag-and-Drop File initialisation
    Setup Drag and Drop Idevice edition and export files for basic functionality.
    
    Modified comfig.xml to express plugin metadata and details
    
    Currently having trouble getting Idevice to show in eXeLearning, using compiler to test in the meantime
    
    Bug fix - Showing in exe learning now
    Idevice can now be seen in eXe learning
    
    Dynamic blank spaces implemented
    Authors can add multiple blanks now
    CSS adjustment
    
    CSS adjustment for author textbox I sleep now

--- plugins/drag-and-drop-idevice/edition/drag-and-drop-idevice.js
+++ plugins/drag-and-drop-idevice/edition/drag-and-drop-idevice.js
@@ -1,194 +1,84 @@
-/**
- * Example iDevice (edition code)
- *
- * Released under Attribution-ShareAlike 4.0 International License.
- * Author: Ignacio Gros (http://gros.es/) for http://exelearning.net/
- *
- * License: http://creativecommons.org/licenses/by-sa/4.0/
- */
 var $exeDevice = {
-	
-	// For score (SCORM) options, see the games' loadSCOFunctions method
-	// i18n intructions: Always use _ as in "Information and instructions."
-	
-	// i18n
-	i18n : {
-		
-		name : _("Example"), // Title in config.xml
-		
-		// Optional translations
-		// Spanish
-		es : {
-			"Information and instructions." : "Información e instrucciones.",
-		}
-		
-	},
+    init: function() {
+        var html = '\
+            <div id="myExampleForm">\
+                <div class="exe-idevice-info">' + _("Instructions: Enter the question with answers surrounded by asterisks.") + '</div>\
+                <p>\
+                    <label for="questionInput">Question:</label>\
+                    <input type="text" id="questionInput" placeholder="Hello yesterday I went to the *shop* then I went *outside*" style="width: 100%; max-width: 600px; height: 40px;">\
+                </p>\
+                <button type="button" id="generateDragDrop">Generate Drag and Drop</button>\
+                <div id="dragDropArea"></div>\
+            </div>\
+        ';
 
-	// Custom i18n (see Advanced mode - Language settings tab)
-	ci18n: {
-		"btn1": _("Click here to start"),
-		"btn2": _("Maximize")
-	},
-	
-	init : function(){
-		
-		// Create the form
-		var html = '\
-			<div id="myExampleForm">\
-				<div class="exe-idevice-info">'+_("Instructions")+'</div>\
-				<div class="exe-form-tab" title="Tab 1 title">\
-					<p>\
-						<label for="myExampleFieldA">Paragraph content:</label>\
-						<input type="text" id="myExampleFieldA">\
-					</p>\
-					<p>\
-						<label for="myExampleFieldB">Paragraph color:</label>\
-						<input type="text" id="myExampleFieldB" class="exe-color-picker">\
-					</p>\
-					<div>\
-						<label for="myExampleFieldC">TinyMCE example:</label>\
-						<textarea id="myExampleFieldC" class="exe-html-editor"></textarea>\
-					</div>\
-					<p>\
-						<label for="myExampleFieldD">File picker example:</label>\
-						<input type="text" id="myExampleFieldD" class="exe-file-picker">\
-					</p>\
-					<p>\
-						<label for="myExampleFieldE">Image picker example:</label>\
-						<input type="text" id="myExampleFieldE" class="exe-image-picker">\
-					</p>\
-				</div>\
-				<div class="exe-form-tab" title="Tab 2 title">\
-					<fieldset class="exe-fieldset exe-fieldset-closed">\
-						<legend><a href="#">Fieldset example</a></legend>\
-						<div>\
-							<p>...</p>\
-						</div>\
-					</fieldset>\
-				</div>\
-				' + $exeAuthoring.iDevice.gamification.common.getLanguageTab(this.ci18n) + '\
-			</div>\
-		';
-		
-		var field = $("#activeIdevice textarea.jsContentEditor");
-		
-		field.before(html); // Hidden TEXTAREA
-		
-		$exeAuthoring.iDevice.tabs.init("myExampleForm"); // Enable tabs
-		
-		this.getPreviousValues(field);
-		
-	},
-	save : function(){
-		
-		// This method will return the content to save (HTML, JSON...)
-		// That code will be placed in the hidden TEXTAREA
-		var res = "";
-		
-		// Paragraph content (with validation example)
-		var p = $("#myExampleFieldA").val();
-		if (p=="") {
-			eXe.app.alert("Please type the paragraph content.");
-			return false;
-		}
+        var field = $("#activeIdevice textarea.jsContentEditor");
+        field.before(html);
+        $exeAuthoring.iDevice.tabs.init("myExampleForm");
 
-		// Paragraph color
-		var pStyle = '';
-		var pC = $("#myExampleFieldB").val();
-		if (pC!="") {
-			pStyle = ' style="color:#'+pC+'"';
-		}		
-		
-		// Paragraph
-		res += "<p"+pStyle+">"+p+"</p>";
-		
-		// Rich text (TinyMCE)
-		var t = tinymce.editors[0].getContent();
-		if (t!="") {
-			res += '<div class="exe-text">';
-				res += t;
-			res += '</div>';	
-		}
-		
-		// File picker
-		var file = $("#myExampleFieldD").val();
-		if (file!="") {
-			var fileName = file;
-				fileName = fileName.split("_");
-				fileName = fileName[fileName.length-1];
-				fileName = fileName.split("/");
-				fileName = fileName[fileName.length-1];
-				if (fileName=="") fileName = "Fichero adjunto";
-			res += '<p class="exe-attachment-file"><a href="'+file+'">'+fileName+'</a></p>';
-		}
-		
-		// Image picker
-		var img = $("#myExampleFieldE").val();
-		if (img!="") {
-			res += '<p class="exe-attachment-img"><img src="'+img+'" alt=""></p>';
-		}
-		
-		// Buttons with custom text (Advanced mode - Language settings tab
-		var btn1 = $("#ci18n_btn1").val();
-		if (btn1=="") btn1 = this.ci18n.btn1;
-		var btn2 = $("#ci18n_btn2").val();
-		if (btn2=="") btn2 = this.ci18n.btn2;
-		
-		res += '<p class="exe-custom-btns">\
-			<button type="button">'+btn1+'</button>\
-			<button type="button">'+btn2+'</button>\
-		</p>';
-		
-		// HTML to save
-		return res;
-	},
-	getPreviousValues : function(field){
-		
-		var content = field.val();
-		if (content != '') {
-			
-			// Find the previous values in the HTML you saved
-			var wrapper = $("<div></div>");
-				wrapper.html(content);
-			
-			// Paragraph
-			var p = $("p",wrapper).eq(0);
-			
-			$("#myExampleFieldA").val(p.text()); // Contenido del párrafo
-			
-			// Paragraph color
-			var pC = p.attr("style");
-			if (typeof(pC)=='string'&&pC.indexOf("color:#")==0) pC = pC.replace("color:#","");
-			$("#myExampleFieldB").val(pC);
-			
-			// TinyMCE
-			var t = $("div.exe-text",wrapper).eq(0);
-			if (t.length==1) {
-				$("#myExampleFieldC").val(t.html());
-			}
-			
-			// Attached file (file picker)
-			var a = $("p.exe-attachment-file a",wrapper).eq(0);
-			if (a.length==1) {
-				$("#myExampleFieldD").val(a.attr("href"));
-			}
-			
-			// Image (image picker)
-			var a = $("p.exe-attachment-img img",wrapper).eq(0);
-			if (a.length==1) {
-				$("#myExampleFieldE").val(a.attr("src"));
-			}
-			
-			// Buttons
-			var btns = $("p.exe-custom-btns button",wrapper);
-			if (btns.length==2) {
-				var btn1 = btns.eq(0).text();
-				var btn2 = btns.eq(1).text();
-				$("#ci18n_btn1").val(btn1);
-				$("#ci18n_btn2").val(btn2);
-			}
-			
-		}
-		
-	}
+        $("#generateDragDrop").on("click", function() {
+            var inputText = $("#questionInput").val();
+            $exeDevice.processInput(inputText);
+        });
+
+        this.getPreviousValues(field);
+    },
+
+    processInput: function(inputText) {
+        var matches = [...inputText.matchAll(/\*(.*?)\*/g)];
+        var lastIndex = 0;
+        var dragDropHtml = '';
+
+        matches.forEach(function(match, index) {
+            var answer = match[1];
+            var start = match.index;
+            var end = start + match[0].length;
+
+            // Add text before the answer
+            dragDropHtml += inputText.substring(lastIndex, start);
+
+            // Add drop zone and draggable item
+            dragDropHtml += '<div class="drop-zone">[Drop answer here]</div>';
+            dragDropHtml += '<div class="draggable" draggable="true" id="draggableAnswer' + index + '">' + answer + '</div>';
+
+            lastIndex = end;
+        });
+
+        // Add remaining text after the last answer
+        dragDropHtml += inputText.substring(lastIndex);
+
+        $("#dragDropArea").html(dragDropHtml);
+
+        // Initialize draggable and droppable functionality
+        this.setupDragAndDrop();
+    },
+
+    setupDragAndDrop: function() {
+        $(".draggable").on("dragstart", function(e) {
+            e.originalEvent.dataTransfer.setData("text/plain", e.target.id);
+        });
+
+        $(".drop-zone").on("dragover", function(e) {
+            e.preventDefault();
+        }).on("drop", function(e) {
+            e.preventDefault();
+            var data = e.originalEvent.dataTransfer.getData("text");
+            var draggableElement = document.getElementById(data);
+            e.target.innerHTML = ''; // Clear the drop zone before appending
+            e.target.appendChild(draggableElement);
+        });
+    },
+
+    save: function() {
+        var res = $("#dragDropArea").html(); // Save the current state of the drag and drop area
+        return res;
+    },
+
+    getPreviousValues: function(field) {
+        var content = field.val();
+        if (content != '') {
+            $("#dragDropArea").html(content);
+            this.setupDragAndDrop(); // Reinitialize drag and drop functionality after loading saved values
+        }
+    }
 }
--- plugins/drag-and-drop-idevice/export/drag-and-drop-idevice.js
+++ plugins/drag-and-drop-idevice/export/drag-and-drop-idevice.js
@@ -1,81 +1,33 @@
-/**
- * Example iDevice
- *
- * Released under Attribution-ShareAlike 4.0 International License.
- * Author: Ignacio Gros (http://gros.es/) for http://exelearning.net/
- *
- * License: http://creativecommons.org/licenses/by-sa/4.0/
- *
- * eXeLearning KIDS logo (export/example-idevice-logo.png) by Francisco Javier Pulido Cuadrado, under CC BY-SA too.
- */
-var $myExampleIdevice = {
-	
-	// Anything included in the export folder will be exported if the iDevice is used.
-	// Vanilla JS or jQuery. Do whatever you need.
-    
-	init : function(){
-        
-		$(".myExampleIdevice").each(function(){
-			
-			// Find the activity wrapper's ID
-			var div = $(".iDevice_content",this);
-			if (div.length!=1) return;
-			var id = div.attr("id");
-			
-			// Make the buttons do something
-			var btns = $(".exe-custom-btns button",this);
-			if (btns.length!=2) return;
-			
-			btns.eq(0).on("click",function(){
-				alert("This is just an example.");
-			});
-			
-			btns.eq(1).on("click",function(){
-				$myExampleIdevice.toggleFullscreen(document.getElementById(id));
-			});
-			
-        });
-		
-	},
-	
-	exitFullscreen: function () {
-		if (document.exitFullscreen) {
-			document.exitFullscreen();
-		} else if (document.msExitFullscreen) {
-			document.msExitFullscreen();
-		} else if (document.mozCancelFullScreen) {
-			document.mozCancelFullScreen();
-		} else if (document.webkitExitFullscreen) {
-			document.webkitExitFullscreen();
-		}
-	},
-	
-	getFullscreen: function (element) {
-		if (element.requestFullscreen) {
-			element.requestFullscreen();
-		} else if (element.mozRequestFullScreen) {
-			element.mozRequestFullScreen();
-		} else if (element.webkitRequestFullscreen) {
-			element.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
-		} else if (element.msRequestFullscreen) {
-			element.msRequestFullscreen();
-		}
-	},
-	
-	toggleFullscreen: function (element) {
-		var element = element || document.documentElement;
-		if (!document.fullscreenElement && !document.mozFullScreenElement &&
-			!document.webkitFullscreenElement && !document.msFullscreenElement) {
-			$myExampleIdevice.getFullscreen(element);
-		} else {
-			$myExampleIdevice.exitFullscreen(element);
-		}
-	}	
-    
-}
+var $exeDeviceExport = {
+    init: function() {
+        // Ensure all drag and drop setups are initialized on document ready
+        this.initializeDragAndDrops();
+    },
 
-$(function(){
-	
-	$myExampleIdevice.init();
-	
+    initializeDragAndDrops: function() {
+        // Iterate over each dragDropArea if there are multiple instances
+        $(".dragDropArea").each(function() {
+            var $area = $(this);
+            
+            // Initialize drag and drop functionality
+            $area.find(".draggable").attr("draggable", "true")
+                .on("dragstart", function(e) {
+                    e.originalEvent.dataTransfer.setData("text", e.target.id);
+                });
+
+            $area.find(".drop-zone").on("dragover", function(e) {
+                e.preventDefault(); // Necessary to allow the drop
+            }).on("drop", function(e) {
+                e.preventDefault();
+                const data = e.originalEvent.dataTransfer.getData("text");
+                const draggable = document.getElementById(data);
+                e.target.innerHTML = ''; // Optionally clear the drop zone before appending
+                e.target.appendChild(draggable);
+            });
+        });
+    }
+};
+
+$(function() {
+    $exeDeviceExport.init();
 });
