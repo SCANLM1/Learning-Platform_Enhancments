85b459b6716bc2a4d1e12b6b8350a2eee69620c6
Joël Hillmann <joelraine8@gmail.com>
2025-04-08 19:09:35 +1200

    CSS and formatting changes
    Separate container for draggable elements

--- plugins/drag-and-drop-idevice/edition/drag-and-drop-idevice.js
+++ plugins/drag-and-drop-idevice/edition/drag-and-drop-idevice.js
@@ -5,7 +5,7 @@ var $exeDevice = {
                 <div class="exe-idevice-info">' + _("Instructions: Enter the question with answers surrounded by asterisks.") + '</div>\
                 <p>\
                     <label for="questionInput">Question:</label>\
-                    <input type="text" id="questionInput" placeholder="Hello yesterday I went to the *shop* then I went *outside*" style="width: 100%; max-width: 600px; height: 40px;">\
+                    <input type="text" id="questionInput" placeholder="Hello yesterday I went to the *shop* then I went *outside*" style="width: 100%; max-width: 1200px; height: 400px;">\
                 </p>\
                 <button type="button" id="generateDragDrop">Generate Drag and Drop</button>\
                 <div id="dragDropArea"></div>\
@@ -24,31 +24,65 @@ var $exeDevice = {
         this.getPreviousValues(field);
     },
 
+    // processInput: function(inputText) {
+    //     var matches = [...inputText.matchAll(/\*(.*?)\*/g)];
+    //     var lastIndex = 0;
+    //     var dragDropHtml = '';
+
+    //     matches.forEach(function(match, index) {
+    //         var answer = match[1];
+    //         var start = match.index;
+    //         var end = start + match[0].length;
+
+    //         // Add text before the answer
+    //         dragDropHtml += inputText.substring(lastIndex, start);
+
+    //         // Add drop zone and draggable item
+    //         dragDropHtml += '<div class="drop-zone">[Drop answer here]</div>';
+    //         dragDropHtml += '<div class="draggable" draggable="true" id="draggableAnswer' + index + '">' + answer + '</div>';
+
+    //         lastIndex = end;
+    //     });
+
+    //     // Add remaining text after the last answer
+    //     dragDropHtml += inputText.substring(lastIndex);
+
+    //     $("#dragDropArea").html(dragDropHtml);
+
+    //     // Initialize draggable and droppable functionality
+    //     this.setupDragAndDrop();
+    // },
+
     processInput: function(inputText) {
         var matches = [...inputText.matchAll(/\*(.*?)\*/g)];
         var lastIndex = 0;
         var dragDropHtml = '';
-
+        var draggableItemsHtml = '<div id="draggableContainer" style="padding: 20px; border: 1px solid #ccc;">';
+    
         matches.forEach(function(match, index) {
             var answer = match[1];
             var start = match.index;
             var end = start + match[0].length;
-
+    
             // Add text before the answer
             dragDropHtml += inputText.substring(lastIndex, start);
-
-            // Add drop zone and draggable item
+    
+            // Add drop zone
             dragDropHtml += '<div class="drop-zone">[Drop answer here]</div>';
-            dragDropHtml += '<div class="draggable" draggable="true" id="draggableAnswer' + index + '">' + answer + '</div>';
-
+    
+            // Add draggable item to a separate container
+            draggableItemsHtml += '<div class="draggable" draggable="true" id="draggableAnswer' + index + '">' + answer + '</div>';
+    
             lastIndex = end;
         });
-
-        // Add remaining text after the last answer
+    
+        // Close the draggable container and add remaining text
+        draggableItemsHtml += '</div>';
         dragDropHtml += inputText.substring(lastIndex);
-
-        $("#dragDropArea").html(dragDropHtml);
-
+    
+        // Combine the zones and draggables in the HTML
+        $("#dragDropArea").html(draggableItemsHtml + dragDropHtml);
+    
         // Initialize draggable and droppable functionality
         this.setupDragAndDrop();
     },
@@ -70,6 +104,8 @@ var $exeDevice = {
     },
 
     save: function() {
+        var inputText = $("#questionInput").val();
+        $exeDevice.processInput(inputText);
         var res = $("#dragDropArea").html(); // Save the current state of the drag and drop area
         return res;
     },
@@ -82,3 +118,4 @@ var $exeDevice = {
         }
     }
 }
+
