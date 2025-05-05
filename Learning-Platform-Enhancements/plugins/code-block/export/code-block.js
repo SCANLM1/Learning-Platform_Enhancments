33333d4b5de9d4eca98f1c9fd6ddb005caf2430f
MaseTheStudent <163777907+MaseTheStudent@users.noreply.github.com>
2025-05-05 17:20:40 +1200

    v3
    added code to export the contnet specified in the code mirror block

--- Learning-Platform-Enhancements/plugins/code-block/export/code-block.js
+++ Learning-Platform-Enhancements/plugins/code-block/export/code-block.js
@@ -1,81 +1,72 @@
 /**
- * Example iDevice
+ * CodeBlock Export iDevice
+ *
+ * Renders the saved XML code block as a read-only CodeMirror editor.
  *
  * Released under Attribution-ShareAlike 4.0 International License.
- * Author: Ignacio Gros (http://gros.es/) for http://exelearning.net/
+ * Author: Your Name for http://exelearning.net/
  *
  * License: http://creativecommons.org/licenses/by-sa/4.0/
- *
- * eXeLearning KIDS logo (export/example-idevice-logo.png) by Francisco Javier Pulido Cuadrado, under CC BY-SA too.
  */
-var $myExampleIdevice = {
-	
-	// Anything included in the export folder will be exported if the iDevice is used.
-	// Vanilla JS or jQuery. Do whatever you need.
-    
-	init : function(){
-        
-		$(".myExampleIdevice").each(function(){
-			
-			// Find the activity wrapper's ID
-			var div = $(".iDevice_content",this);
-			if (div.length!=1) return;
-			var id = div.attr("id");
-			
-			// Make the buttons do something
-			var btns = $(".exe-custom-btns button",this);
-			if (btns.length!=2) return;
-			
-			btns.eq(0).on("click",function(){
-				alert("This is just an example.");
-			});
-			
-			btns.eq(1).on("click",function(){
-				$myExampleIdevice.toggleFullscreen(document.getElementById(id));
-			});
-			
-        });
-		
+var $codeBlockIdevice = {
+	// Entry point
+	init: function() {
+	var self = this;
+	this.loadDependencies(function(){
+		self.renderCodeBlocks();
+	});
 	},
-	
-	exitFullscreen: function () {
-		if (document.exitFullscreen) {
-			document.exitFullscreen();
-		} else if (document.msExitFullscreen) {
-			document.msExitFullscreen();
-		} else if (document.mozCancelFullScreen) {
-			document.mozCancelFullScreen();
-		} else if (document.webkitExitFullscreen) {
-			document.webkitExitFullscreen();
-		}
-	},
-	
-	getFullscreen: function (element) {
-		if (element.requestFullscreen) {
-			element.requestFullscreen();
-		} else if (element.mozRequestFullScreen) {
-			element.mozRequestFullScreen();
-		} else if (element.webkitRequestFullscreen) {
-			element.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
-		} else if (element.msRequestFullscreen) {
-			element.msRequestFullscreen();
-		}
-	},
-	
-	toggleFullscreen: function (element) {
-		var element = element || document.documentElement;
-		if (!document.fullscreenElement && !document.mozFullScreenElement &&
-			!document.webkitFullscreenElement && !document.msFullscreenElement) {
-			$myExampleIdevice.getFullscreen(element);
-		} else {
-			$myExampleIdevice.exitFullscreen(element);
-		}
-	}	
-    
-}
 
-$(function(){
-	
-	$myExampleIdevice.init();
-	
+	// Dynamically inject CodeMirror CSS/JS into <head>
+	loadDependencies: function(callback) {
+	var head = document.getElementsByTagName("head")[0];
+
+	// Core CSS
+	var link1 = document.createElement("link");
+	link1.rel = "stylesheet";
+	link1.href = "https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.1/codemirror.min.css";
+	head.appendChild(link1);
+
+	// Monokai theme
+	var link2 = document.createElement("link");
+	link2.rel = "stylesheet";
+	link2.href = "https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.1/theme/monokai.min.css";
+	head.appendChild(link2);
+
+	// Core JS
+	var script1 = document.createElement("script");
+	script1.src = "https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.1/codemirror.min.js";
+	script1.onload = function() {
+		// XML mode
+		var script2 = document.createElement("script");
+		script2.src = "https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.1/mode/xml/xml.min.js";
+		script2.onload = callback;
+		head.appendChild(script2);
+	};
+	head.appendChild(script1);
+	},
+
+	// Find each saved <pre> and replace with read-only CodeMirror
+	renderCodeBlocks: function() {
+	// `.codeBlockIdevice` must match your iDevice wrapper class
+	$(".codeBlockIdevice .iDevice_content div.exe-code-block pre").each(function() {
+		var preEl = this;
+		var code = preEl.textContent;
+		CodeMirror(function(node) {
+		preEl.parentNode.replaceChild(node, preEl);
+		}, {
+		value: code,
+		mode: "xml",
+		theme: "monokai",
+		readOnly: "nocursor",
+		lineNumbers: true,
+		lineWrapping: true
+		});
+	});
+	}
+};
+
+// Initialize on DOM ready
+$(function() {
+	$codeBlockIdevice.init();
 });
