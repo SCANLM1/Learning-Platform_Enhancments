1292944ae2f892605dbaad69f1f68ce495527388
MaseTheStudent <163777907+MaseTheStudent@users.noreply.github.com>
2025-05-05 22:20:36 +1200

    v4 added security
    added security fucntion to construct a meta tag as it cannot be done manually withthe type of html construction that exeLeaning does

--- Learning-Platform-Enhancements/plugins/code-block/edition/code-block.js
+++ Learning-Platform-Enhancements/plugins/code-block/edition/code-block.js
@@ -3,17 +3,35 @@
  */
 var $exeDevice = {
 	i18n: { name: _("CodeBlock") },
+
+	// reference to our code mirror instance
 	codeMirrorEditor: null,
-  
+
 	init: function() {
-	  var self = this;
-	  this.loadDependencies(function() {
-		// 1) Build the CodeMirror form
+	var self = this;
+
+	// inject content security policy 
+	// has to be done because rendering html like normal does not work
+	// simple <meta> tag has to be rendered within a function
+	(function(){
+		var meta = document.getElement("meta");
+		meta.httpEquiv = "Content-security-Policy";
+		meta.content = ""
+			+ "default-src 'self'; "
+			+ "script-src 'self' https://cdnja.cloudflare.com; "
+			+ "style-src 'self' https://cdnja.cloudflare.com; "
+			+ "object-src 'none'; "
+			+ "sandbox";
+		document.head.appendChild(meta);
+	})();
+
+	this.loadDependencies(function() {
+		// Build the CodeMirror form
 		var html = '\
-		  <div id="codeBlockForm">\
+		<div id="codeBlockForm">\
 			<div class="exe-idevice-info">'+ _("Code you want to present: ") +'</div>\
 			<textarea id="codeBlockEditor"></textarea>\
-		  </div>';
+		</div>';
 		// 2) Insert it before the hidden textarea
 		var field = $("#activeIdevice textarea.jsContentEditor");
 		field.before(html);
@@ -22,19 +40,19 @@ var $exeDevice = {
 		// 4) Init CodeMirror
 		var area = document.getElementById("codeBlockEditor");
 		if (area) {
-		  self.codeMirrorEditor = CodeMirror.fromTextArea(area, {
+		self.codeMirrorEditor = CodeMirror.fromTextArea(area, {
 			lineNumbers: true,
 			mode: "javascript",
 			theme: "monokai",
 			lineWrapping: true
-		  });
+		});
 		}
-	  });
+	});
 	},
-  
+
 	loadDependencies: function(cb) {
-	  var head = document.head;
-	  if (typeof CodeMirror === "undefined") {
+	var head = document.head;
+	if (typeof CodeMirror === "undefined") {
 		// Core CSS
 		var l1 = document.createElement("link");
 		l1.rel = "stylesheet";
@@ -49,45 +67,44 @@ var $exeDevice = {
 		var s1 = document.createElement("script");
 		s1.src = "https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.1/codemirror.min.js";
 		s1.onload = function() {
-		  // JavaScript mode
-		  var s2 = document.createElement("script");
-		  s2.src = "https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.1/mode/javascript/javascript.min.js";
-		  s2.onload = cb;
-		  head.appendChild(s2);
+		// JavaScript mode
+		var s2 = document.createElement("script");
+		s2.src = "https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.1/mode/javascript/javascript.min.js";
+		s2.onload = cb;
+		head.appendChild(s2);
 		};
 		head.appendChild(s1);
-	  } else cb();
+	} else cb();
 	},
-  
+
 	save: function() {
-	  var res = "";
-	  if (this.codeMirrorEditor) {
+	var res = "";
+	if (this.codeMirrorEditor) {
 		var c = this.codeMirrorEditor.getValue();
 		if (c) res = '<div class="exe-code-block"><pre>' + c + '</pre></div>';
-	  }
-	  // Store into the hidden editor field
-	  $("#activeIdevice textarea.jsContentEditor").val(res);
-	  return res;
+	}
+	// Store into the hidden editor field
+	$("#activeIdevice textarea.jsContentEditor").val(res);
+	return res;
 	},
-  
+
 	getPreviousValues: function(field) {
-	  var content = field.val();
-	  if (content) {
+	var content = field.val();
+	if (content) {
 		var wrapper = $("<div>").html(content);
 		var pre = wrapper.find("div.exe-code-block pre").get(0);
 		if (pre) {
-		  var text = pre.textContent;
-		  $("#codeBlockEditor").val(text);
+		var text = pre.textContent;
+		$("#codeBlockEditor").val(text);
 		}
-	  }
 	}
-  };
-  
-  // Translation stub
-  function _(s){ return s; }
-  
-  // Kick off edition
-  $(document).ready(function(){
+	}
+};
+
+// Translation stub
+function _(s){ return s; }
+
+// Kick off edition
+$(document).ready(function(){
 	$exeDevice.init();
-  });
-  
+});
